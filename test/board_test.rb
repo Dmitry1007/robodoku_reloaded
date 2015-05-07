e62f5767d665657d00d63c4005dcdaa995547628
Max Tedford <max.tedford@gmail.com>
2015-05-07 11:27:55 -0600

    continuing to beef up the tests to ensure proper coverage

--- test/board_test.rb
+++ test/board_test.rb
@@ -15,16 +15,52 @@ class BoardTest < Minitest::Test
   def test_it_has_spots
     assert_equal 81, @board.spots.size
   end
+  
+  def test_it_can_return_the_spots_values
+    assert_equal 0, @board.spots[0].value
+    assert_equal 6, @board.spots[40].value
+    assert_equal 1, @board.spots[80].value
+  end
+  
+  def test_it_can_return_the_spots_starting_possibilities
+    assert_equal [1, 2, 3, 4, 5, 6, 7, 8, 9], @board.spots[0].possibilities
+    assert_equal [1, 2, 3, 4, 5, 6, 7, 8, 9], @board.spots[40].possibilities
+    assert_equal [1, 2, 3, 4, 5, 6, 7, 8, 9], @board.spots[80].possibilities
+  end
 
   def test_it_has_rows
     assert_equal 9, @board.rows.size
+  end
+  
+  def test_it_can_return_the_values_of_its_rows
+    assert_equal [0, 2, 6, 5, 9, 4, 3, 1, 7], @board.rows[0].spots.map(&:value)
+    assert_equal [7, 1, 5, 6, 3, 8, 9, 4, 2], @board.rows[1].spots.map(&:value)
+    assert_equal [3, 9, 4, 7, 2, 1, 8, 6, 5], @board.rows[2].spots.map(&:value)
   end
   
   def test_it_has_columns
     assert_equal 9, @board.rows.size
   end
   
+  def test_it_can_return_the_values_of_its_columns
+    assert_equal [5, 6, 7, 4, 2, 8, 9, 1, 3], @board.columns[3].spots.map(&:value)
+    assert_equal [9, 3, 2, 5, 6, 1, 4, 7, 8], @board.columns[4].spots.map(&:value)
+    assert_equal [4, 8, 1, 9, 7, 3, 2, 6, 5], @board.columns[5].spots.map(&:value)
+  end
+  
   def test_it_has_squares
     assert_equal 9, @board.rows.size
+  end
+  
+  def test_it_can_return_the_values_of_its_squares
+    assert_equal [5, 4, 6, 3, 8, 7, 1, 2, 9], @board.squares[6].spots.map(&:value)
+    assert_equal [9, 1, 3, 4, 7, 8, 2, 6, 5], @board.squares[7].spots.map(&:value)
+    assert_equal [7, 5, 4, 8, 3, 2, 6, 9, 1], @board.squares[8].spots.map(&:value)
+  end
+  
+  def test_it_can_tell_if_all_spots_have_been_decided
+    refute @board.all_decided?
+    @board.evaluate_all_possibilities
+    assert @board.all_decided?
   end
 end
--- test/solver_test.rb
+++ test/solver_test.rb
@@ -17,17 +17,22 @@ class SolverTest < Minitest::Test
 
   def test_it_can_contain_characters
     assert_equal ["026594317", "715638942", "394721865", "163459278", "948267153", "257813694", "531942786", "482176539", "679385421"], @solver.board.rows.map{|row| row.spots.map{|spot| spot.value}}.map{|ele| ele.join}
+    assert_equal ["073192546", "219645387", "654387129", "567428913", "932561478", "481973265", "398216754", "146759832", "725834691"], @solver.board.columns.map{|column| column.spots.map{|spot| spot.value}}.map{|ele| ele.join}
+    assert_equal ["073219654", "567932481", "398146725", "192645387", "428561973", "216759834", "546387129", "913478265", "754832691"], @solver.board.squares.map{|square| square.spots.map{|spot| spot.value}}.map{|ele| ele.join}
   end
 
   def test_it_solves_the_simplest_possible_puzzle
+    skip
     assert_equal ["826594317", "715638942", "394721865", "163459278", "948267153", "257813694", "531942786", "482176539", "679385421"], @solver.solve
   end
 
   def test_it_solves_a_slightly_harder_puzzle
+    skip
     assert_equal ["826594317", "715638942", "394721865", "163459278", "948267153", "257813694", "531942786", "482176539", "679385421"], @solver2.solve
   end
 
   def test_it_solves_an_even_harder_puzzle
+    skip
     assert_equal ["826594317", "715638942", "394721865", "163459278", "948267153", "257813694", "531942786", "482176539", "679385421"], @solver3.solve
   end
   

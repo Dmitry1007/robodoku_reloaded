a687cb9a8c2513a1f14a1b6c07d7ec1c20cbeaf4
Max Tedford <max.tedford@gmail.com>
2015-05-07 10:18:31 -0600

    adjusting the syntax in test files

--- test/column_test.rb
+++ test/column_test.rb
@@ -9,18 +9,18 @@ class ColumnTest < Minitest::Test
   end
 
   def test_it_has_spots
-    row = Column.new([Spot.new(0), Spot.new(0), Spot.new(2), Spot.new(7), Spot.new(0), Spot.new(1), Spot.new(9), Spot.new(0), Spot.new(0)])
-    assert_equal [0, 0, 2, 7, 0, 1, 9, 0, 0], row.spots.map{|spot| spot.value}
+    column = Column.new([Spot.new(0), Spot.new(0), Spot.new(2), Spot.new(7), Spot.new(0), Spot.new(1), Spot.new(9), Spot.new(0), Spot.new(0)])
+    assert_equal [0, 0, 2, 7, 0, 1, 9, 0, 0], column.spots.map{|spot| spot.value}
   end
 
   def test_it_has_undecided_spots
-    row = Column.new([Spot.new(0), Spot.new(0), Spot.new(2), Spot.new(7), Spot.new(0), Spot.new(1), Spot.new(9), Spot.new(0), Spot.new(0)])
-    assert_equal [0, 0, 0, 0, 0], row.undecided_spots.map{|spot| spot.value}
+    column = Column.new([Spot.new(0), Spot.new(0), Spot.new(2), Spot.new(7), Spot.new(0), Spot.new(1), Spot.new(9), Spot.new(0), Spot.new(0)])
+    assert_equal [0, 0, 0, 0, 0], column.undecided_spots.map{|spot| spot.value}
   end
 
   def test_it_can_assess_itself
-    row = Column.new([Spot.new(0), Spot.new(2), Spot.new(3), Spot.new(4), Spot.new(5), Spot.new(6), Spot.new(7), Spot.new(8), Spot.new(9)])
-    row.assess_column
-    assert_equal 1, row.spots.first.value
+    column = Column.new([Spot.new(0), Spot.new(2), Spot.new(3), Spot.new(4), Spot.new(5), Spot.new(6), Spot.new(7), Spot.new(8), Spot.new(9)])
+    column.assess_column
+    assert_equal 1, column.spots.first.value
   end
 end
--- test/square_test.rb
+++ test/square_test.rb
@@ -9,18 +9,18 @@ class SquareTest < Minitest::Test
   end
 
   def test_it_has_spots
-    row = Square.new([Spot.new(0), Spot.new(0), Spot.new(2), Spot.new(7), Spot.new(0), Spot.new(1), Spot.new(9), Spot.new(0), Spot.new(0)])
-    assert_equal [0, 0, 2, 7, 0, 1, 9, 0, 0], row.spots.map{|spot| spot.value}
+    square = Square.new([Spot.new(0), Spot.new(0), Spot.new(2), Spot.new(7), Spot.new(0), Spot.new(1), Spot.new(9), Spot.new(0), Spot.new(0)])
+    assert_equal [0, 0, 2, 7, 0, 1, 9, 0, 0], square.spots.map{|spot| spot.value}
   end
 
   def test_it_has_undecided_spots
-    row = Square.new([Spot.new(0), Spot.new(0), Spot.new(2), Spot.new(7), Spot.new(0), Spot.new(1), Spot.new(9), Spot.new(0), Spot.new(0)])
-    assert_equal [0, 0, 0, 0, 0], row.undecided_spots.map{|spot| spot.value}
+    square = Square.new([Spot.new(0), Spot.new(0), Spot.new(2), Spot.new(7), Spot.new(0), Spot.new(1), Spot.new(9), Spot.new(0), Spot.new(0)])
+    assert_equal [0, 0, 0, 0, 0], square.undecided_spots.map{|spot| spot.value}
   end
 
   def test_it_can_assess_itself
-    row = Square.new([Spot.new(0), Spot.new(2), Spot.new(3), Spot.new(4), Spot.new(5), Spot.new(6), Spot.new(7), Spot.new(8), Spot.new(9)])
-    row.assess_square
-    assert_equal 1, row.spots.first.value
+    square = Square.new([Spot.new(0), Spot.new(2), Spot.new(3), Spot.new(4), Spot.new(5), Spot.new(6), Spot.new(7), Spot.new(8), Spot.new(9)])
+    square.assess_square
+    assert_equal 1, square.spots.first.value
   end
 end
